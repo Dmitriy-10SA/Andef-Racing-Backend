f3cd57bb47af8aabe63f8d935057c5ef21c69937
dsemkin <semkin_dmitriy10@vk.com>
2025-06-07 01:11:32 +0400

    Club common ok

--- src/main/kotlin/model/Employee.kt
+++ src/main/kotlin/model/Employee.kt
@@ -4,8 +4,6 @@ object Employee {
     const val TABLE_NAME = "employee"
 
     const val ID = "id"
-    const val CLUB_ID = "club_id"
-    const val EMPLOYEE_TYPE_ID = "employee_type_id"
     const val SURNAME = "surname"
     const val NAME = "name"
     const val PHONE = "phone"
